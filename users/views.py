a1198b0b643ba3680feea7afe2cd6ba03480e319
Josh Stewart <josh.stewart@awesomeinc.org>
2022-04-26 13:16:15 +0000

    Update gitpod, small adjustments to models

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import status, permissions
+from rest_framework import status, permissions, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import action
@@ -9,6 +9,7 @@ from .serializers import CustomUserSerializer
 
 class UserCreate(APIView):
     permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
 
     def post(self, request, format='json'):
         serializer = CustomUserSerializer(data=request.data)
@@ -18,3 +19,8 @@ class UserCreate(APIView):
                 json = serializer.data
                 return Response(json, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserDetail(generics.RetrieveAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
